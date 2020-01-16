c64814611ce9316365b3f4d3af3bdb5652630e8e
Rahmat Eka <rahmat_eka@me.com>
2020-01-16 13:11:14 +0700

    added sort

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, Fragment, useRef } from 'react'
 import StackGrid from "react-stack-grid"
-import { Empty, Card, Skeleton, Loading, Avatar } from 'antd'
+import { Empty, Card, Skeleton, Loading, Avatar, Row, Col, Select } from 'antd'
 import useAxios from '../hooks/useAxios'
 import useUrlBuilder from '../hooks/useUrlBuilder'
 import '../App.css'
 import {timeAgo, toDollars, generateAdsId} from '../helpers'
 
 const { Meta } = Card
+const { Option } = Select
 
 const List = ()=> {
   const [deleteId, setDeleteId] = useState(null)
   const [params, setParams] = useState({
     _page: 1,
     _limit: 15,
+    _sort: 'date',
     _max: 100,
   })
 
@@ -69,46 +71,43 @@ const List = ()=> {
     fetch()
   }, [params])
 
-  const prevScrollY = useRef(0);
+  const prevScrollY = useRef(0)
 
-  const [goingUp, setGoingUp] = useState(false);
+  const [goingUp, setGoingUp] = useState(false)
 
-  const limitScroll = Math.max( document.body.scrollHeight) - 401;
+  const limitScroll = Math.max( document.body.scrollHeight) - 401
 
   useEffect(() => {
     const handleScroll = () => {
-      const currentScrollY = window.scrollY;
+      const currentScrollY = window.scrollY
       if (prevScrollY.current < currentScrollY && goingUp) {
-        setGoingUp(false);
+        setGoingUp(false)
       }
       if (prevScrollY.current > currentScrollY && !goingUp) {
-        setGoingUp(true);
+        setGoingUp(true)
       }
-      prevScrollY.current = currentScrollY;
-      // console.log(goingUp, currentScrollY);
+      prevScrollY.current = currentScrollY
+      // console.log(goingUp, currentScrollY)
       // console.log(limitScroll)
       trackScrolling()
-    };
-    window.addEventListener("scroll", handleScroll, { passive: true });
+    }
+    window.addEventListener("scroll", handleScroll, { passive: true })
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [goingUp]);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [goingUp])
 
   const isBottom = (el) => {
-    return el.getBoundingClientRect().bottom <= window.innerHeight;
+    return el.getBoundingClientRect().bottom <= window.innerHeight
   }
 
   const trackScrolling = () => {
-    const wrappedElement = document.getElementById('scrollcontainer');
+    const wrappedElement = document.getElementById('scrollcontainer')
     if (isBottom(wrappedElement)) {
       if(!listState.isFetching){
-        // console.log(listState.isFetching)
-        // console.log('header bottom reached');
-        // console.log(params._page)
         loadMore()
       }
     }
-  };
+  }
 
   const itemSkeleton = () => {
     return Array.from(Array(params._limit), (e, i) => (
@@ -156,18 +155,56 @@ const List = ()=> {
     ))
   }
 
+  const fetchSort = (value)=>{
+    listSend(
+      {
+        params:{
+          _page: 1,
+          _limit: params._page*params._limit,
+          _sort: value,
+        },
+      },
+      (response) => {
+        setResponse((prevState) => ({
+          ...prevState,
+          results:response,
+        }))
+      }
+    )
+  }
+
+  const changeSort = (value)=> {
+    setParams((prevState) => ({
+      ...prevState,
+      _sort:value,
+    }),fetchSort(value))
+  }
+
+
   return (
     <Fragment>
-      <StackGrid columnWidth={200}>
-        {showItems()}
-        {listState.isFetching && (
-          itemSkeleton()
-        )}
-      </StackGrid>
-      <div id="scrollcontainer">
-      </div>
+      <Row>
+        <Col span={8} offset={17} style={{marginBottom:'10px'}}>
+          Sort By &nbsp;
+          <Select defaultValue="date" style={{ width: 120 }} onChange={changeSort}>
+            <Option value="price">Price</Option>
+            <Option value="date">Date</Option>
+            <Option value="id">Id</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row>
+        <StackGrid columnWidth={200}>
+          {showItems()}
+          {listState.isFetching && (
+            itemSkeleton()
+          )}
+        </StackGrid>
+        <div id="scrollcontainer">
+        </div>
+      </Row>
     </Fragment>
-  );
+  )
 }
 
-export default List;
+export default List
